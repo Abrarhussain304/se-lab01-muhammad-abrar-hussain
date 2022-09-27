6d976100f2bd8452e743da1cf7eff77805d02b84
Abrarhussain304 <abrarhussain2020@namal.edu.pk>
2022-09-27 21:23:54 +0500

    cal03.py file

---
+++ calculator/cal03.py
@@ -0,0 +1,13 @@
+# calculator in pytho(v0.3)
+ def add(num1, num2):
+     return num1+num2
+ def mul(num1, num2):
+     return num1*num2
+ def sub(num1, num2):
+     return num1-num2
+
+#main
+result  = add(5,3)
+print(result)
+print(mul(5,3))
+print(sub(5,3))
