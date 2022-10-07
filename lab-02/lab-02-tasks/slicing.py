7c9b376c721d573141c95fc11f968889608cb9a7
Abrarhussain304 <abrarhussain2020@namal.edu.pk>
2022-10-08 00:43:25 +0500

    lab task added

---
+++ lab-02/lab-02-tasks/slicing.py
@@ -0,0 +1,12 @@
+# String slicing
+namal_uni = "NamalUniversityMianwali"
+print(namal_uni[0:9:2])
+
+# Collaborated by Maria Maqsood
+print(namal_uni[2:10:3])
+print(namal_uni[1:12:4])
+print(namal_uni[4:8:2])
+
+# Updated by Muhammad Abrar Hussain
+print(namal_uni[3:9:2])
+print(namal_uni[1:10:5])
---
+++ lab-02/terminal/square.py
@@ -0,0 +1,7 @@
+def square(num1):
+num1  = int(input('enter number:'))
+square = num1*num1
+print(square)
+
+
+square(num1)
